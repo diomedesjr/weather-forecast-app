481245eee61869147edcb79d92a42db3c6dc4d48
deejay <hinay.diomedesjr@gmail.com>
2021-08-20 17:38:24 +0800

    deleted something

--- src/components/atoms/Loader/style.ts
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components'
-import Backdrop from '@material-ui/core/Backdrop'
-
-export const StyledBackdrop = styled(Backdrop)`
-  color: white;
-  z-index: ${({ theme }) => theme.zIndex.drawer + 1};
-`
